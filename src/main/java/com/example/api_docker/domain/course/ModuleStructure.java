8f2600591b32e9e598145362c00858782af7df44
taliberti <guilhermetaliberti@gmail.com>
2026-08-22 11:34:03 -0300

    Criando tests para o domínio Module

--- src/main/java/com/example/api_docker/domain/course/ModuleStructure.java
+++ src/main/java/com/example/api_docker/domain/course/ModuleStructure.java
@@ -1,32 +1,23 @@
 package com.example.api_docker.domain.course;
 
 import com.example.api_docker.domain.shared.exception.DomainException;
-import lombok.Getter;
 
 import java.util.List;
 import java.util.Objects;
 
-@Getter
-public class ModuleStructure{
+public record ModuleStructure(ModuleId moduleId, String title, List<LessonId> lessons) {
 
-    private final ModuleId moduleId;
-    private final String title;
-    private final List<LessonId> lessons;
-
-    private ModuleStructure (ModuleId moduleId, String title, List<LessonId> lessons) {
-        this.moduleId = moduleId;
-        this.title = title;
-        this.lessons = lessons;
-    }
-
-    public static ModuleStructure of(ModuleId moduleId, String title, List<LessonId> lessons){
+    public ModuleStructure {
         Objects.requireNonNull(moduleId, "ModuleId não pode ser nulo");
-
         Objects.requireNonNull(title, "Title não pode ser nulo");
 
         if (lessons == null || lessons.isEmpty())
             throw new DomainException("Um módulo precisa de ao menos uma lesson.");
 
+        lessons = List.copyOf(lessons);
+    }
+
+    public static ModuleStructure of(ModuleId moduleId, String title, List<LessonId> lessons) {
         return new ModuleStructure(moduleId, title, lessons);
     }
 }
