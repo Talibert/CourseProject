69910fb6410fef1fdfb5ab808f5e797da2c1a381
taliberti <guilhermetaliberti@gmail.com>
2026-08-22 11:38:43 -0300

    Criando tests para o domínio Enrollment

--- src/main/java/com/example/api_docker/domain/enrollment/exception/InvalidEnrollmentTransitionException.java
+++ src/main/java/com/example/api_docker/domain/enrollment/exception/InvalidEnrollmentTransitionException.java
@@ -3,6 +3,7 @@ package com.example.api_docker.domain.enrollment.exception;
 import com.example.api_docker.domain.enrollment.EnrollmentStatusType;
 
 public class InvalidEnrollmentTransitionException extends RuntimeException {
-    public InvalidEnrollmentTransitionException(EnrollmentStatusType status, EnrollmentStatusType enrollmentStatusType) {
+    public InvalidEnrollmentTransitionException(EnrollmentStatusType currentStatus, EnrollmentStatusType targetStatus) {
+        super("Transição de status da matrícula inválida: de %s para %s".formatted(currentStatus, targetStatus));
     }
 }
